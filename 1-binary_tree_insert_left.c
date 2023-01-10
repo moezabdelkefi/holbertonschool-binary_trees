0a7e54a8cfb80ea33b4dbad516d87cbc6214a46f
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-10 03:26:49 -0930

    insert a node as a left child

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newnode;
+    if (parent == NULL)
+        return (NULL);
+
+    newnode = malloc(sizeof(binary_tree_t));
+    if (newnode == NULL)
+        return (NULL);
+    newnode->n = value;
+    newnode->parent = parent;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    if (parent->left != NULL)
+    {
+        newnode->left = parent->left;
+        newnode->left->parent = newnode;
+    }
+    parent->left = newnode;
+    return (newnode);
+}
