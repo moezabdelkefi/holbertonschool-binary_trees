e6717b9aee8e9e497faf92c1142b5bfba62abf84
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-11 02:32:32 -0930

    adding file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,22 +4,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int max = 0;
 
-    if (tree != NULL)
+    size_t l = 0, m = 0;
+    if (tree == NULL)
     {
-        int leftsubtree = binary_tree_height(tree->left);
-        int rightsubtree = binary_tree_height(tree->right);
-        if (leftsubtree > rightsubtree)
-        {
-            max = leftsubtree + 1;
-            return max;
-        }
-        else
-        {
-            max = rightsubtree + 1;
-            return max;
-        }
+        return 0;
     }
-    return (max);
+    if (tree->right)
+
+        l = 1 + binary_tree_height(tree->right);
+    if (tree->left)
+        m = binary_tree_height(tree->left);
+    if (l > m)
+        return (l);
+    else
+        return (m);
 }
