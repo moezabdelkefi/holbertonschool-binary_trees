e5bd022d389c433e9867c64e1567529dee910335
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-11 03:19:40 -0930

    fix betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,6 +2,7 @@
 /**
  * binary_tree_is_leaf-a function that checks if a node is a leaf
  *@node:  is a pointer to the node to check
+ *Return: 1 or 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
