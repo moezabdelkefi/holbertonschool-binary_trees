21173a763643b8a52b493981342e8a681adfc73d
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-11 03:18:52 -0930

    fix betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,6 +2,7 @@
 /**
  *binary_tree_is_root - a function that checks if a given node is a root
  *@node: is a pointer to the node to check
+ *Return: 1 or 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
