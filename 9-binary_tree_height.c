bcd4ddf693049dd35622dce4b6e3f5844ffcf2a9
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-11 02:34:16 -0930

    adding file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,25 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height -
+ * binary_tree_height - a function that measures the height
+ * @tree:  is a pointer to the root node of the tree to measure the height
+ * Return: l or m
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-    size_t l = 0, m = 0;
-    if (tree == NULL)
-    {
-        return 0;
-    }
-    if (tree->right)
+	size_t l = 0, m = 0;
 
-        l = 1 + binary_tree_height(tree->right);
-    if (tree->left)
-        m = binary_tree_height(tree->left);
-    if (l > m)
-        return (l);
-    else
-        return (m);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right)
+
+		l = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+		m = binary_tree_height(tree->left);
+	if (l > m)
+		return (l);
+	else
+		return (m);
 }
