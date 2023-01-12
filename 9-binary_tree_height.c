bfbf0f2e8d89c1212c5eb33fe0c1652e36f0d33b
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-12 03:54:29 -0930

    fix betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-	size_t l = 0, m = 0;
+	size_t left = 0, right = 0;
 
 	if (tree == NULL)
 	{
@@ -15,11 +15,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	if (tree->right)
 
-		l = 1 + binary_tree_height(tree->right);
+		right = 1 + binary_tree_height(tree->right);
 	if (tree->left)
-		m = binary_tree_height(tree->left);
-	if (l > m)
-		return (l);
+		left = binary_tree_height(tree->left);
+	if (right > left)
+		return (right);
 	else
-		return (m);
+		return (left);
 }
