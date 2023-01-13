686691099032f12d0516b135ae41e54baa55fd74
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-13 01:11:43 -0930

    a function that find the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,22 +2,23 @@
 /**
  *binary_tree_sibling - a function that finds the sibling of a node
  *@node: is a pointer to the node to find the sibling
+ *Return: right or left
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
 
 	if (!node || !node->parent)
 	{
 		return (NULL);
 	}
 
-	if (parent->left == node)
+	binary_tree_t *sibling = node->parent;
+	if (sibling->left == node)
 	{
-		return (parent->right);
+		return (sibling->right);
 	}
 	else
 	{
-		return (parent->left);
+		return (sibling->left);
 	}
 }
